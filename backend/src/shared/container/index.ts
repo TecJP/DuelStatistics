9ed8bb6e04de99b1293f60750da0ae6ae95344a9
TecJP <tec.joao.freitas@gmail.com>
2021-01-06 18:51:55 -0300

    Finishing fogotpassword tests

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -9,6 +9,9 @@ import StatisticsRepository from '@modules/statistics/infra/typeorm/repositories
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/';
+
 container.registerSingleton<IStatisticsRepository>(
   'StatisticsRepository',
   StatisticsRepository,
