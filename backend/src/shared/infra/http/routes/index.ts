e84fd4b1a1aff75f97a90f42f99dcc59269ff73f
TecJP <tec.joao.freitas@gmail.com>
2021-01-08 11:33:55 -0300

    Making controllers and routes of forgot and reset password

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import statisticsRouter from '@modules/statistics/infra/http/routes/statistics.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/statistics', statisticsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
