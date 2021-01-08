e11e09a88f7aaacce51ffa04fc7953f448260c49
TecJP <tec.joao.freitas@gmail.com>
2021-01-08 15:58:41 -0300

    Finishing the recover of password

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -32,11 +32,11 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists!');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(
+    await this.mailProvider.sendMail(
       email,
-      'Pedido de Recuperação de senha!',
+      `Pedido de Recuperação de senha: ${token}`,
     );
   }
 }
